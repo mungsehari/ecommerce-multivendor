d2e46c2bb491413138569918b7068415d6e0e3b0
mungsehari <mungse.hari@gmail.com>
2024-10-15 16:27:30 +0530

    update VerificationCodeRepository

--- src/main/java/com/hari/repository/VerificationCodeRepository.java
+++ src/main/java/com/hari/repository/VerificationCodeRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
 
     VerificationCode findByEmail(String email);
+
+    VerificationCode findByOtp(String otp);
 }
 
