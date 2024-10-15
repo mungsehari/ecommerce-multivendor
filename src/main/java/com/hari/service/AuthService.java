fbf92aeae39d8e8fa05b0f44969013575f12625b
mungsehari <mungse.hari@gmail.com>
2024-10-15 16:07:32 +0530

    update AuthService

--- src/main/java/com/hari/service/AuthService.java
+++ src/main/java/com/hari/service/AuthService.java
@@ -1,11 +1,12 @@
 package com.hari.service;
 
+import com.hari.domain.USER_ROLE;
 import com.hari.request.LoginRequest;
 import com.hari.request.SignupRequest;
 import com.hari.response.AuthResponse;
 
 public interface AuthService {
-    void sendLoginOtp(String email) throws Exception;
+    void sendLoginOtp(String email, USER_ROLE role) throws Exception;
     String createUser(SignupRequest request) throws Exception;
-    AuthResponse signing(LoginRequest request);
+    AuthResponse signing(LoginRequest request) throws Exception;
 }
