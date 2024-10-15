01ca6f9cb1782d020382b829df6378c052dca905
mungsehari <mungse.hari@gmail.com>
2024-10-15 16:25:44 +0530

    public SellerRepository

--- src/main/java/com/hari/repository/SellerRepository.java
+++ src/main/java/com/hari/repository/SellerRepository.java
@@ -1,8 +1,12 @@
 package com.hari.repository;
 
+import com.hari.domain.AccountStatus;
 import com.hari.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SellerRepository extends JpaRepository<Seller,Long> {
     Seller findByEmail(String email);
+    List<Seller> findByAccountStatus(AccountStatus status);
 }
