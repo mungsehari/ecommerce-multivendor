b13211307a738a7313239a8693ece6fc4be9ef45
mungsehari <mungse.hari@gmail.com>
2024-10-15 16:17:58 +0530

    create OrderController

---
+++ src/main/java/com/hari/controller/OrderController.java
@@ -0,0 +1,103 @@
+package com.hari.controller;
+
+import com.hari.domain.PaymentMethod;
+import com.hari.model.*;
+import com.hari.repository.PaymentOrderRepository;
+import com.hari.response.PaymentLinkResponse;
+import com.hari.service.*;
+import com.razorpay.PaymentLink;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+    private final UserService userService;
+    private final CartService cartService;
+    private final SellerService sellerService;
+    private final SellerReportService sellerReportService;
+    private final PaymentService paymentService;
+    private final PaymentOrderRepository paymentOrderRepository;
+
+
+
+@PostMapping
+public ResponseEntity<PaymentLinkResponse> createOrderHandler(
+        @RequestBody Address spippingAddress,
+        @RequestParam PaymentMethod paymentMethod,
+        @RequestHeader("Authorization") String jwt) throws Exception {
+    User user=userService.findUserByJwtToken(jwt);
+
+    Cart cart=cartService.findUserCart(user);
+    Set<Order> orders=orderService.createOrder(user,spippingAddress,cart);
+    PaymentOrder paymentOrder=paymentService.createOrder(user,orders);
+    PaymentLinkResponse response=new PaymentLinkResponse();
+
+    if (paymentMethod.equals(PaymentMethod.RAZORPAY)){
+        PaymentLink payment=paymentService.createRazorpayPaymentLink(user,
+                paymentOrder.getAmount(),
+                paymentOrder.getId());
+        String paymentUrl=payment.get("short_url");
+        String paymentLinkId=payment.get("id");
+        response.setPayment_link_url(paymentUrl);
+
+        paymentOrder.setPaymentLinkId(paymentLinkId);
+        paymentOrderRepository.save(paymentOrder);
+    }else {
+        String paymentUrl=paymentService.createStripePaymentLink(user,
+                paymentOrder.getAmount(),
+                paymentOrder.getId());
+        response.setPayment_link_url(paymentUrl);
+    }
+
+    return new ResponseEntity<>(response,HttpStatus.OK);
+}
+
+@GetMapping("/user")
+public ResponseEntity<List<Order>> usersOrderHistoryHandler(
+    @RequestHeader("Authorization") String jwt
+) throws Exception {
+    User user=userService.findUserByJwtToken(jwt);
+
+    List<Order> orders=orderService.userOrderHistory(user.getId());
+    return new ResponseEntity<>(orders, HttpStatus.ACCEPTED);
+}
+
+@GetMapping("/{orderId}")
+public ResponseEntity<Order> getOrderById(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws Exception {
+    User user=userService.findUserByJwtToken(jwt);
+    Order order=orderService.findOrderById(orderId);
+
+    return new ResponseEntity<>(order,HttpStatus.ACCEPTED);
+
+}
+@GetMapping("/item/{orderItemId}")
+public ResponseEntity<OrderItem> getOrderItemById(@PathVariable Long orderItemId,@RequestHeader("Authorization") String jwt) throws Exception {
+    User user=userService.findUserByJwtToken(jwt);
+    OrderItem orderItem=orderService.getOrderItemById(orderItemId);
+    return new ResponseEntity<>(orderItem,HttpStatus.ACCEPTED);
+}
+
+@PutMapping("/{orderId}/cancel")
+public ResponseEntity<Order> cancelOrder(@PathVariable Long orderId,@RequestHeader("Authorization") String jwt) throws Exception {
+    User user=userService.findUserByJwtToken(jwt);
+    Order order=orderService.cancelOrder(orderId,user);
+
+    Seller seller=sellerService.getSellerById(order.getSellerId());
+
+    SellerReport report=sellerReportService.getSellerReport(seller);
+    report.setCancelledOrders(report.getCancelledOrders());
+    report.setTotalRefunds(report.getTotalRefunds()+order.getTotalSellingPrice());
+    sellerReportService.updateSellerReport(report);
+    return ResponseEntity.ok(order);
+}
+
+}
