aa7cb5acaeb2ba1133ef3701baef0b8aef9f12e9
mungsehari <mungse.hari@gmail.com>
2024-10-15 16:17:40 +0530

    update LoginOtpRequest

---
+++ src/main/java/com/hari/request/LoginOtpRequest.java
@@ -0,0 +1,11 @@
+package com.hari.request;
+
+import com.hari.domain.USER_ROLE;
+import lombok.Data;
+
+@Data
+public class LoginOtpRequest {
+    private String email;
+    private String otp;
+    private USER_ROLE role;
+}
