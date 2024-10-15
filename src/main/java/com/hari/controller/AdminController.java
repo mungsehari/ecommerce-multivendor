774ac6a12c4dda03e8ce1826ce24598737dff611
mungsehari <mungse.hari@gmail.com>
2024-10-15 16:06:59 +0530

    Create a AdminController

---
+++ src/main/java/com/hari/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.hari.controller;
+
+import com.hari.domain.AccountStatus;
+import com.hari.model.Seller;
+import com.hari.service.SellerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AdminController {
+    private final SellerService sellerService;
+
+    @PatchMapping("/seller/{id}/status/{status}")
+    public ResponseEntity<Seller> updateSellerStatus(
+            @PathVariable Long id,
+            @PathVariable AccountStatus status
+    ) throws Exception {
+        Seller seller=sellerService.updateSellerAccountStatus(id,status);
+        return ResponseEntity.ok(seller);
+
+    }
+}
